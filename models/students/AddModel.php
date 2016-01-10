3d4dc1800b1eb08ab34bd65f3cfa0663ab357f52
Pichot Benjamin <pichotb@free.fr>
2016-01-10 23:52:12 +0100

    studentsaddmodel

--- models/students/AddModel.php
+++ models/students/AddModel.php
@@ -88,10 +88,14 @@
 									 $ADRESSE, 
 									 $NUM_TEL, 
 									 $DATE_NAISSANCE, 									
-									 $LIEU_ETUDE) {
+									 $LIEU_ETUDE
+									 $FK_FORMULES
+									 $FK_MONITEUR
+									 $FK_FACTURE) {
 			try {
 				//UPDATE ELEVE SET FK_MONITEUR = '21', NOM = 'bi', PRENOM = 'jay', ADRESSE = 'no whi', NUM_TEL = 4, DATE_NAISSANCE = TO_DATE('2016-01-17', 'YYYY-MM-DD HH24:MI:SS'), LIEU_ETUDE = 'se' WHERE PK_ELEVE =21
-				$qry = oci_parse($this->db, ("INSERT INTO AUTO.ELEVE  (NOM, PRENOM, ADRESSE, NUM_TEL, DATE_NAISSANCE, LIEU_ETUDE)  VALUES (':NOM', ':PRENOM', ':ADRESSE',:NUM_TEL, TO_DATE(':DATE_NAISSANCE', 'YYYY-MM-DD HH24:MI:SS'),':LIEU_ETUDE')");
+				$qry = oci_parse($this->db, ("INSERT INTO AUTO.ELEVE  (NOM, PRENOM, ADRESSE, NUM_TEL, DATE_NAISSANCE, LIEU_ETUDE, FK_FORMULES,FK_MONITEUR,FK_FACTURE)  
+				VALUES (:NOM, :PRENOM, :ADRESSE,:NUM_TEL, TO_DATE(:DATE_NAISSANCE, \'YYYY-MM-DD HH24:MI:SS\'),:LIEU_ETUDE,:FK_FORMULES,:FK_MONITEUR,:FK_FACTURE)");
 							
 				oci_bind_by_name($qry,":NOM",$NOM);
 				oci_bind_by_name($qry,":PRENOM",$PRENOM);	
@@ -99,6 +103,9 @@
 				oci_bind_by_name($qry,":NUM_TEL",$NUM_TEL);
 				oci_bind_by_name($qry,":DATE_NAISSANCE",$DATE_NAISSANCE);	
 				oci_bind_by_name($qry,":LIEU_ETUDE",$LIEU_ETUDE);
+				oci_bind_by_name($qry,":FK_FORMULES",$FK_FORMULES);
+				oci_bind_by_name($qry,":FK_MONITEUR",$FK_MONITEUR);
+				oci_bind_by_name($qry,":FK_FACTURE",$FK_FACTURE);
 
 				oci_execute($qry);
 				oci_close($this->db);
